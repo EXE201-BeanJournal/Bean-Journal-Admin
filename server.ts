e0f5b9d1433f520b0860be5a4101805117213dd1
Nguyen Dinh Duong <DuongNDSE183456@fpt.edu.vn>
2025-06-13 17:02:25 +0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express, { RequestHandler } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import os from 'os';
 import getAllUsersHandler, {
   getUserByIdHandler,
   updateUserByIdHandler,
@@ -31,7 +32,19 @@ app.patch('/api/users/:userId', typedUpdateUserByIdHandler);  // PATCH update a 
 app.delete('/api/users/:userId', typedDeleteUserByIdHandler); // DELETE a user by ID
 app.get('/api/users/:userId/sessions', typedGetUserSessionsHandler); // Add route for user sessions
 
+const getNetworkAddress = () => {
+  for (const interfaceDetails of Object.values(os.networkInterfaces())) {
+    if (!interfaceDetails) continue;
+    for (const details of interfaceDetails) {
+      const { family, address, internal } = details;
+      if (family === 'IPv4' && !internal) return address;
+    }
+  }
+  return 'localhost';
+};
+
 app.listen(PORT, () => {
-  console.log(`Backend server is running on http://localhost:${PORT}`);
+  const host = getNetworkAddress();
+  console.log(`Backend server is running on http://${host}:${PORT}`);
   console.log('Ensure your CLERK_SECRET_KEY is set in your .env file.');
 }); 
