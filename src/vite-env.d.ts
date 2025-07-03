922ac6699cb32ba482d744adf7955d9526ff1c15
Nguyen Dinh Duong <DuongNDSE183456@fpt.edu.vn>
2025-07-03 20:51:29 +0700

    ghf

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string
+  readonly VITE_SUPABASE_ANON_KEY: string
+  readonly VITE_EMAIL_SERVICE_URL: string
+  readonly VITE_EMAIL_SERVICE_KEY: string
+  readonly VITE_EMAIL_FROM_ADDRESS: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
